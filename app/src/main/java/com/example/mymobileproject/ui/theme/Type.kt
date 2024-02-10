90ed4c80ebbbebca4f888e898afcb10d02f025bc
dxrtyx <dartcomand@gmail.com>
2024-02-10 23:11:15 +0200

    add drawables and init fonts

--- app/src/main/java/com/example/mymobileproject/ui/theme/Type.kt
+++ app/src/main/java/com/example/mymobileproject/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.mymobileproject.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.mymobileproject.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,9 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val robotoFontFamily = FontFamily(
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium)
 )
